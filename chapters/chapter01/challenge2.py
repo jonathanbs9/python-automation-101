bc647b27873f8d417fdc03652704ed22b29e9868
Jonathan Brull Schroeder <jonathans@avalith.net>
2021-08-19 12:08:13 -0300

    Chapter 1 - challenge2

---
+++ chapters/chapter01/challenge2.py
@@ -0,0 +1,6 @@
+season_year = input('Season of the year: ')
+number = input('What is your favorite number?: ')
+adjective = input('Type an adjective: ')
+
+
+print(f'On an {adjective} {season_year} day, I drink at least {number} cups of juice! ')
